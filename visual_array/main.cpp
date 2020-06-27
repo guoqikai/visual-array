600e0edfdacaf15427ab4058996a3209dfcc6323
guoqikai <guosidney@gmail.com>
2020-06-27 12:22:37 -0400

    Initial Commit

---
+++ visual_array/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  visual_array
+//
+//  Created by Qikai Guo on 2020-06-27.
+//  Copyright © 2020 Qikai Guo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
